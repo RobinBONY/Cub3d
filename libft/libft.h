46165d526d5f67c75199d7742a5ce56af35a329a
Robin Bony <rbony@z1r3p4.42lyon.fr>
2022-08-11 11:56:23 +0200

    ft_strstr

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: rbony <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 15:58:45 by rbony             #+#    #+#             */
-/*   Updated: 2022/08/11 10:57:51 by rbony            ###   ########lyon.fr   */
+/*   Updated: 2022/08/11 11:54:39 by rbony            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,7 @@ int			ft_strcmp(const char *s1, const char *s2);
 int			ft_strncmp(const char *s1, const char *s2, size_t n);
 void		*ft_memchr(const void *s, int c, size_t n);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
+char		*ft_strstr(const char *haystack, const char *needle);
 char		*ft_strnstr(const char *haystack, const char *needle, size_t n);
 int			ft_atoi(const char *str);
 void		*ft_calloc(size_t count, size_t size);
